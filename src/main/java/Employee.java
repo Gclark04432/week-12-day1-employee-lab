2ec8d1619ecb16f644a0f95fb84e9efa7687ac2d
Gary Clark <gclark04432@live.co.uk>
2020-02-17 14:22:48 +0000

    abstract class employee setup

---
+++ src/main/java/Employee.java
@@ -0,0 +1,32 @@
+public class Employee {
+    private String name;
+    private String NINumber;
+    private int salary;
+
+    public Employee(String name, String niNumber, int salary){
+        this.name = name;
+
+        NINumber = niNumber;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNINumber() {
+        return NINumber;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void raiseSalary(double increase){
+        this.salary *= increase;
+    }
+
+    public double payBonus(){
+        return this.salary * 0.01;
+    }
+}
